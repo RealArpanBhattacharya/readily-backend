894607e63fe7f1d7c32e3534f1d809c22e370b6e
Arpan Bhattacharya <arpan@Arpans-MBP-10.home>
2020-11-15 06:40:29 -0600

    output domain

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nope' });
+  res.send({body: JSON.stringify(req.get('host'))});
 });
 
 
